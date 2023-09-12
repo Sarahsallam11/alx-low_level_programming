77cfc10bf1055185e32c3b2c7bc7dfa081cb8433
sarah <flowersoso96@hotmail.com>
2023-09-12 13:57:09 -0700

    bye

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,19 +3,30 @@
 /**
  * main - Entry point
  *
- * Description: prints _putchar using putchar prototype
+ * Description: prints the first 50 Fibonacci numbers
+ *		starting with 1 and 2 followed by new line
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
+	int count;
+	unsigned long fib1 = 0, fibe2 = 1, sum;
 
-	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
-	_putchar('\n');
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		_putchar("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf('\n');
+		else
+			printf(", ");
+	}
 
 	return (0);
 }
